3bebf6f638c5ec872320723f839e8451388538ea
dsopata <dawidsopata@yahoo.com>
2021-04-22 02:06:42 +0200

    Revert "added cards"
    This reverts commit a9862f35ab5548688ca5aeac844d739d7a92d272.

--- Gameplay.py
+++ Gameplay.py
@@ -2,10 +2,10 @@ from Card import Card
 from Deck import Deck
 from Player import Player
 
+suits_symbol = {'X':'', 'BUST!':'', 'Hearts':'\u2665', 'Diamonds':'\u2666', 'Spades':'\u2660', 'Clubs':'\u2663'}
 
 
 class Gameplay:
-    suits_symbol = {'X':'', 'BUST!':'', 'Hearts':'\u2665', 'Diamonds':'\u2666', 'Spades':'\u2660', 'Clubs':'\u2663'}
 
     def __init__(self):
 
@@ -65,12 +65,12 @@ class Gameplay:
         print('\n'*50)
         print(f"{self.dealer.name}: ")
         for card in self.dealer.hand:
-            print(card.rank + self.suits_symbol[card.suit] + ' ', end='')
+            print(card.rank + suits_symbol[card.suit] + ' ', end='')
         print('\n'*3)
         for contestant in self.list_of_players:
             print(f"{contestant.name}: ")
             for card in contestant.hand:
-                print(card.rank + self.suits_symbol[card.suit] + ' ', end='')
+                print(card.rank + suits_symbol[card.suit] + ' ', end='')
             print('\n')
 
     def player_hit_or_stop(self, deck, contestant):
